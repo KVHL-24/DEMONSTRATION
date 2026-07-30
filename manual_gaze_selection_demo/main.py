879d132983d7eb841cfda4dfe8c3d99fa52ed2f5
jonathanswinnen <46630860+jonathanswinnen@users.noreply.github.com>
2026-07-30 15:13:42 +0200

    small fix

--- manual_gaze_selection_demo/main.py
+++ manual_gaze_selection_demo/main.py
@@ -7,16 +7,16 @@ Usage:
 import sys
 from PySide6.QtWidgets import QApplication
 
-from data_stream import SyntheticDataPipeline
+from data_pipeline import SyntheticDataPipeline
 from gui import GUI
 
 
 def main():
 
     stream = SyntheticDataPipeline(
-        "data/directional_near_dynamic_taz+022_iaz-032_snr+0_rep09/array_audio.wav", 
-        "data/directional_near_dynamic_taz+022_iaz-032_snr+0_rep09/gaze.npy",
-        "data/directional_near_dynamic_taz+022_iaz-032_snr+0_rep09/metadata.json"
+        "../../../data/directional_near_dynamic_taz+022_iaz-032_snr+0_rep09/array_audio.wav", 
+        "../../../data/directional_near_dynamic_taz+022_iaz-032_snr+0_rep09/gaze.npy",
+        "../../../data/directional_near_dynamic_taz+022_iaz-032_snr+0_rep09/metadata.json"
     )
 
     app = QApplication(sys.argv)
